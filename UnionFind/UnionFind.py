03369ef0fbd9e40a6dbb7fb0a3ca4266bbdc8484
titan <59005133+titanium-22@users.noreply.github.com>
2022-11-22 20:04:35 +0900

    Update and rename UnionFind.py to UnionFind/UnionFind.py

--- UnionFind/UnionFind.py
+++ UnionFind/UnionFind.py
@@ -1,3 +1,6 @@
+# https://github.com/titanium-22/Library/blob/main/UnionFind.py
+
+
 from collections import defaultdict
 
 
@@ -7,15 +10,17 @@ class UnionFind:
   def __init__(self, n: int):
     self.__n = n
     self.__group_numbers = n
-    self._parents = [-1] * n
-    self._G = [[] for _ in range(n)]
+    self.__parents = [-1] * n
+    self.__G = [[] for _ in range(n)]
 
   def __find(self, x: int) -> int:
     a = x
-    while self._parents[a] >= 0:
-      a = self._parents[a]
-    while self._parents[x] >= 0:
-      self._parents[x], x = a, self._parents[x]
+    while self.__parents[a] >= 0:
+      a = self.__parents[a]
+    while self.__parents[x] >= 0:
+      y = x
+      x = self.__parents[x]
+      self.__parents[y] = a
     return a
 
   '''Untie x and y. / O(α(N))'''
@@ -23,21 +28,17 @@ class UnionFind:
     x, y = self.__find(x), self.__find(y)
     if x == y:
       return
-    self._G[x].append(y)
-    self._G[y].append(x)
+    self.__G[x].append(y)
+    self.__G[y].append(x)
     self.__group_numbers -= 1
-    if self._parents[x] > self._parents[y]:
+    if self.__parents[x] > self.__parents[y]:
       x, y = y, x
-    self._parents[x] += self._parents[y]
-    self._parents[y] = x
-
-  '''Return leader or x. / O(α(N))'''
-  def leader(self, x: int) -> int:
-    return self.__find(x)
+    self.__parents[x] += self.__parents[y]
+    self.__parents[y] = x
 
   '''Return xが属する集合の要素数. / O(α(N))'''
   def size(self, x: int) -> int:
-    return -self._parents[self.__find(x)]
+    return -self.__parents[self.__find(x)]
 
   '''Return True if 'same' else False. / O(α(N))'''
   def same(self, x: int, y: int) -> bool:
@@ -49,7 +50,7 @@ class UnionFind:
     todo = [x]
     while todo:
       v = todo.pop()
-      for vv in self._G[v]:
+      for vv in self.__G[v]:
         if vv in seen:
           continue
         todo.append(vv)
@@ -58,7 +59,7 @@ class UnionFind:
 
   '''Return all roots. / O(N)'''
   def roots(self) -> list:
-    return [i for i, x in enumerate(self._parents) if x < 0]
+    return [i for i, x in enumerate(self.__parents) if x < 0]
 
   '''Return the number of groups. / O(1)'''
   def group_count(self) -> int:
@@ -71,9 +72,6 @@ class UnionFind:
       group_members[self.__find(member)].append(member)
     return group_members
 
-  def groups(self) -> list:
-    return list(self.all_group_members().values())
-
   '''relax. / O(Nα(N))'''
   def relax(self) -> None:
     for i in range(self.__n):
@@ -82,8 +80,8 @@ class UnionFind:
   '''Clear. / O(N)'''
   def clear(self) -> None:
     self.__group_numbers = self.__n
-    self._parents = [-1] * self.__n
-    self._G = [[] for _ in range(self.__n)]
+    self.__parents = [-1] * self.__n
+    self.__G = [[] for _ in range(self.__n)]
 
   def __str__(self) -> str:
     ret = '<UnionFind> [\n'
