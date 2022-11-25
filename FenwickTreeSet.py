082f6cd1e05eeb0fe40309e712df17909a356754
titan <59005133+titanium-22@users.noreply.github.com>
2022-11-26 00:50:02 +0900

    Update FenwickTreeSet.py

--- FenwickTreeSet.py
+++ FenwickTreeSet.py
@@ -1,40 +1,43 @@
+# https://github.com/titanium-22/Library/blob/main/FenwickTreeSet.py
+
+
 class FenwickTreeSet:
   
   # Build a new FenwickTreeSet. / O(len(A) log(len(A)))
-  def __init__(self, A: set, V=[], _multi=False):
-    # A: used values(distinct)(needed); V: init values
+  def __init__(self, used: set, a=[], _multi=False):
+    # used: used values(distinct)(needed); a: init values
 
-    __A = sorted(A)
+    _used = sorted(used)
     self._len = 0
-    self.__size = len(__A)
+    self._size = len(_used)
 
-    if __A[-1] != self.__size - 1:
-      self._to_zaatsu = {x: i for i, x in enumerate(__A)}
+    if _used[-1] != self._size - 1:
+      self._to_zaatsu = {x: i for i, x in enumerate(_used)}
     else:
-      self._to_zaatsu = __A
-    self._to_origin = __A
+      self._to_zaatsu = _used
+    self._to_origin = _used
 
-    self._cnt = [0] * self.__size
+    self._cnt = [0] * self._size
 
-    if V:
-      VV = [0] * self.__size
-      for v in V:
+    if a:
+      a_ = [0] * self._size
+      for v in a:
         i = self._to_zaatsu[v]
         if _multi:
           self._len += 1
-          VV[i] += 1
+          a_[i] += 1
           self._cnt[i] += 1
-        elif VV[i] == 0:
+        elif a_[i] == 0:
           self._len += 1
-          VV[i] = 1
+          a_[i] = 1
           self._cnt[i] = 1
-      self._fw = FenwickTree(self.__size, V=VV)
+      self._fw = FenwickTree(a_)
     else:
-      self._fw = FenwickTree(self.__size)
+      self._fw = FenwickTree(self._size)
 
   '''Add x. / O(logN)'''
-  def add(self, x) -> bool:
-    i = self._to_zaatsu[x]
+  def add(self, key) -> bool:
+    i = self._to_zaatsu[key]
     if self._cnt[i] == 0:
       self._len += 1
       self._cnt[i] = 1
@@ -43,14 +46,14 @@ class FenwickTreeSet:
     return False
 
   '''Remove x. / O(logN)'''
-  def remove(self, x) -> bool:
-    if self.discard(x):
+  def remove(self, key) -> bool:
+    if self.discard(key):
       return True
-    raise KeyError(x)
+    raise KeyError(key)
 
   '''Discard x. / O(logN)'''
-  def discard(self, x) -> bool:
-    i = self._to_zaatsu[x]
+  def discard(self, key) -> bool:
+    i = self._to_zaatsu[key]
     if self._cnt[i] == 0:
       return False
     self._len -= 1
@@ -58,52 +61,52 @@ class FenwickTreeSet:
     self._fw.add(i, -1)
     return True
 
-  '''Return the number of x. / O(logN)'''
-  def count(self, x) -> int:
-    return self._cnt[self._to_zaatsu[x]]
+  '''Return the number of key. / O(logN)'''
+  def count(self, key) -> int:
+    return self._cnt[self._to_zaatsu[key]]
 
-  '''Find the largest element <= x, or None if it doesn't exist. / O(logN)'''
-  def le(self, x):
-    i = self._to_zaatsu[x]
+  '''Find the largest element <= key, or None if it doesn't exist. / O(logN)'''
+  def le(self, key):
+    i = self._to_zaatsu[key]
     if self._cnt[i]:
-      return x
+      return key
     pref = self._fw.pref(i - 1) - 1
     return None if pref < 0 else self.__getitem__(pref)
 
-  '''Find the largest element < x, or None if it doesn't exist. / O(logN)'''
-  def lt(self, x):
-    pref = self._fw.pref(self._to_zaatsu[x] - 1) - 1
+  '''Find the largest element < key, or None if it doesn't exist. / O(logN)'''
+  def lt(self, key):
+    pref = self._fw.pref(self._to_zaatsu[key] - 1) - 1
     return None if pref < 0 else self.__getitem__(pref)
 
-  '''Find the smallest element >= x, or None if it doesn't exist. / O(logN)'''
-  def ge(self, x):
-    i = self._to_zaatsu[x]
+  '''Find the smallest element >= key, or None if it doesn't exist. / O(logN)'''
+  def ge(self, key):
+    i = self._to_zaatsu[key]
     if self._cnt[i] > 0:
-      return x
+      return key
     pref = self._fw.pref(i)
     return None if pref >= self._len else self.__getitem__(pref)
 
-  '''Find the smallest element > x, or None if it doesn't exist. / O(logN)'''
-  def gt(self, x):
-    pref = self._fw.pref(self._to_zaatsu[x])
+  '''Find the smallest element > key, or None if it doesn't exist. / O(logN)'''
+  def gt(self, key):
+    pref = self._fw.pref(self._to_zaatsu[key])
     return None if pref >= self._len else self.__getitem__(pref)
 
-  '''Count the number of elements < x. / O(logN)'''
-  def index(self, x) -> int:
-    return self._fw.pref(self._to_zaatsu[x] - 1)
+  '''Count the number of elements < key. / O(logN)'''
+  def index(self, key) -> int:
+    return self._fw.pref(self._to_zaatsu[key] - 1)
 
-  '''Count the number of elements <= x. / O(logN)'''
-  def index_right(self, x) -> int:
-    return self._fw.pref(self._to_zaatsu[x])
+  '''Count the number of elements <= key. / O(logN)'''
+  def index_right(self, key) -> int:
+    return self._fw.pref(self._to_zaatsu[key])
 
   '''Return and Remove max element or a[p]. / O(logN)'''
   def pop(self, p=-1):
     if p < 0:
       p += self._len
     self._len -= 1
-    i, acc, s = 0, 0, 1 << self._fw._depth
+    i, acc, s = 0, 0, self._fw._s
     while s:
-      if i + s <= self.__size:
+      if i + s <= self._size:
         if acc + self._fw._tree[i + s] <= p:
           acc += self._fw._tree[i + s]
           i += s
@@ -120,26 +123,23 @@ class FenwickTreeSet:
   def __getitem__(self, x):
     if x < 0:
       x += self._len
-    if 0 <= x < self._len:
-      return self._to_origin[self._fw.bisect_right(x)]
-    raise IndexError
+    return self._to_origin[self._fw.bisect_right(x)]
 
   def __repr__(self):
-    # return '<FenwickTreeSet> {' + ', '.join(map(str, self)) + '}'
-    return self.__str__()
+    return 'FenwickTreeSet ' + str(self)
 
   def __str__(self):
     return '{' + ', '.join(map(str, self)) + '}'
 
   def __iter__(self):
-    self.__iter = 0
+    self._iter = 0
     return self
 
   def __next__(self):
-    if self.__iter == self._len:
+    if self._iter == self._len:
       raise StopIteration
-    res = self.__getitem__(self.__iter)
-    self.__iter += 1
+    res = self.__getitem__(self._iter)
+    self._iter += 1
     return res
 
   def __reversed__(self):
@@ -158,8 +158,8 @@ class FenwickTreeSet:
 
 class FenwickTreeMultiSet(FenwickTreeSet):
 
-  def __init__(self, A: set, V=[]) -> None:
-    super().__init__(A, V=V, _multi=True)  # A: set or HashSet
+  def __init__(self, used: set, a=[]) -> None:
+    super().__init__(used, a, _multi=True)
 
   '''Add x. / O(logN)'''
   def add(self, x: int, num=1) -> None:
@@ -185,20 +185,20 @@ class FenwickTreeMultiSet(FenwickTreeSet):
 
   '''Discard all x. / O(logN)'''
   def discard_all(self, x: int) -> bool:
-    return self.discard(x, num=super().count(x))
+    return self.discard(x, num=self.count(x))
 
   '''Yield (key, val). / O(NlogN)'''
   def items(self) -> tuple:
-    __iter = 0
-    while __iter < self._len:
-      res = self.__getitem__(__iter)
-      cnt = super().count(res)
-      __iter += cnt
+    _iter = 0
+    while _iter < self._len:
+      res = self.__getitem__(_iter)
+      cnt = self.count(res)
+      _iter += cnt
       yield (res, cnt)
 
   def show(self):
     print('{' + ', '.join(f'{i[0]}: {i[1]}' for i in self.items()) + '}')
 
   def __repr__(self):
-    return self.__str__()
+    return 'FenwickTreeMultiSet ' + str(self)
 
