3da274e1d7cece42d982e25363106245aa980b46
titan <59005133+titanium-22@users.noreply.github.com>
2022-12-06 12:44:57 +0900

    Update ModInt.py

--- Math/ModInt.py
+++ Math/ModInt.py
@@ -5,7 +5,7 @@ from typing import Union
 from functools import lru_cache
 
 
-class ModInt:
+class mint:
 
   # mod = 1000000007
   mod = 998244353
@@ -16,78 +16,60 @@ class ModInt:
     return pow(a, self.mod-2, self.mod)
 
   def __init__(self, val: int) -> None:
-    self.val = val if 0 <= val and val < self.mod else val%self.mod
+    self.val = val % self.mod
 
-  def __add__(self, other: Union[int, "ModInt"]) -> "ModInt":
-    val = self.val + (other if isinstance(other, int) else other.val)
-    if val > self.mod: val -= self.mod
-    return ModInt(val)
+  def __add__(self, other: Union[int, "mint"]) -> "mint":
+    return mint(self.val + (other if isinstance(other, int) else other.val))
 
-  def __sub__(self, other: Union[int, "ModInt"]) -> "ModInt":
-    val = self.val - (other if isinstance(other, int) else other.val)
-    if val < 0: val += self.mod
-    return ModInt(val)
+  def __sub__(self, other: Union[int, "mint"]) -> "mint":
+    return mint(self.val - (other if isinstance(other, int) else other.val))
 
-  def __mul__(self, other: Union[int, "ModInt"]) -> "ModInt":
-    val = self.val * (other if isinstance(other, int) else other.val)
-    return ModInt(val)
+  def __mul__(self, other: Union[int, "mint"]) -> "mint":
+    return mint(self.val * (other if isinstance(other, int) else other.val))
 
-  def __truediv__(self, other: Union[int, "ModInt"]) -> "ModInt":
-    val = self.val * (self._inv(other) if isinstance(other, int) else self._inv(other.val))
-    return ModInt(val)
+  def __truediv__(self, other: Union[int, "mint"]) -> "mint":
+    return mint(self.val * (self._inv(other) if isinstance(other, int) else self._inv(other.val)))
 
-  def __radd__(self, other: Union[int, "ModInt"]) -> "ModInt":
-    val = (other if isinstance(other, int) else other.val) + self.val
-    if val > self.mod: val -= self.mod
-    return ModInt(val)
+  def __radd__(self, other: Union[int, "mint"]) -> "mint":
+    return mint((other if isinstance(other, int) else other.val) + self.val)
 
-  def __rsub__(self, other: Union[int, "ModInt"]) -> "ModInt":
-    val = (other if isinstance(other, int) else other.val) - self.val
-    if val < 0: val += self.mod
-    return ModInt(val)
+  def __rsub__(self, other: Union[int, "mint"]) -> "mint":
+    return mint((other if isinstance(other, int) else other.val) - self.val)
 
-  def __rmul__(self, other: Union[int, "ModInt"]) -> "ModInt":
-    val = (other if isinstance(other, int) else other.val) * self.val
-    return ModInt(val)
+  def __rmul__(self, other: Union[int, "mint"]) -> "mint":
+    return mint((other if isinstance(other, int) else other.val) * self.val)
 
-  def __rtruediv__(self, other: Union[int, "ModInt"]) -> "ModInt":
-    val = (other if isinstance(other, int) else other.val) * self._inv(self.val)
-    return ModInt(val)
+  def __rtruediv__(self, other: Union[int, "mint"]) -> "mint":
+    return mint((other if isinstance(other, int) else other.val) * self._inv(self.val))
 
-  def __iadd__(self, other: Union[int, "ModInt"]) -> "ModInt":
-    val = self.val + (other if isinstance(other, int) else other.val)
-    if val > self.mod: val -= self.mod
-    return ModInt(val)
+  def __iadd__(self, other: Union[int, "mint"]) -> "mint":
+    return mint(self.val + (other if isinstance(other, int) else other.val))
 
-  def __isub__(self, other: Union[int, "ModInt"]) -> "ModInt":
-    val = self.val - (other if isinstance(other, int) else other.val)
-    if val < 0: val -= self.mod
-    return ModInt(val)
+  def __isub__(self, other: Union[int, "mint"]) -> "mint":
+    return mint(self.val - (other if isinstance(other, int) else other.val))
 
-  def __imul__(self, other: Union[int, "ModInt"]) -> "ModInt":
-    val = self.val * (other if isinstance(other, int) else other.val)
-    return ModInt(val)
+  def __imul__(self, other: Union[int, "mint"]) -> "mint":
+    return mint(self.val * (other if isinstance(other, int) else other.val))
 
-  def __itruediv__(self, other: Union[int, "ModInt"]) -> "ModInt":
-    val = self.val * (self._inv(other) if isinstance(other, int) else self._inv(other.val))
-    return ModInt(val)
+  def __itruediv__(self, other: Union[int, "mint"]) -> "mint":
+    return mint(self.val * (self._inv(other) if isinstance(other, int) else self._inv(other.val)))
 
-  def __eq__(self, other: Union[int, "ModInt"]):
+  def __eq__(self, other: Union[int, "mint"]):
     return int(self) == int(other)
 
-  def __lt__(self, other: Union[int, "ModInt"]):
+  def __lt__(self, other: Union[int, "mint"]):
     return int(self) < int(other)
 
-  def __le__(self, other: Union[int, "ModInt"]):
+  def __le__(self, other: Union[int, "mint"]):
     return int(self) <= int(other)
 
-  def __gt__(self, other: Union[int, "ModInt"]):
+  def __gt__(self, other: Union[int, "mint"]):
     return int(self) > int(other)
 
-  def __ge__(self, other: Union[int, "ModInt"]):
+  def __ge__(self, other: Union[int, "mint"]):
     return int(self) >= int(other)
 
-  def __ne__(self, other: Union[int, "ModInt"]):
+  def __ne__(self, other: Union[int, "mint"]):
     return int(self) != int(other)
   
   def __int__(self):
@@ -99,4 +81,3 @@ class ModInt:
   def __repr__(self):
     return str(self)
 
-  
