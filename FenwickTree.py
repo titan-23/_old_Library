63487c1b1233f2352c005858da4f561860557df6
titan <59005133+titanium-22@users.noreply.github.com>
2022-12-06 22:33:43 +0900

    Update FenwickTree.py

--- FenwickTree.py
+++ FenwickTree.py
@@ -1,17 +1,20 @@
 # https://github.com/titanium-22/Library/blob/main/FenwickTree.py
 
 
+from typing import List, Union
+
+
 class FenwickTree:
 
   '''Build a new FenwickTree. / O(N)'''
-  def __init__(self, _n_or_a):
+  def __init__(self, _n_or_a: Union[List[int], int]):
     if isinstance(_n_or_a, int):
       self._size = _n_or_a
       self._tree = [0] * (self._size+1)
     else:
       a = list(_n_or_a)
       self._size = len(a)
-      self._tree[1:] = a
+      self._tree = [0] + a
       for i in range(1, self._size):
         if i + (i & -i) <= self._size:
           self._tree[i + (i & -i)] += self._tree[i]
@@ -37,16 +40,16 @@ class FenwickTree:
     assert 0 <= l <= r <= self._size
     return self.pref(r - 1) - self.pref(l - 1)
 
-  def __getitem__(self, i: int) -> int:
-    return self.sum(i, i+1)
+  def __getitem__(self, k: int) -> int:
+    return self.sum(k, k+1)
 
-  '''Add x to a[p]. / O(logN)'''
-  def add(self, p: int, x: int) -> None:
-    assert 0 <= p < self._size
-    p += 1
-    while p <= self._size:
-      self._tree[p] += x
-      p += p & -p
+  '''Add x to a[k]. / O(logN)'''
+  def add(self, k: int, x: int) -> None:
+    assert 0 <= k < self._size
+    k += 1
+    while k <= self._size:
+      self._tree[k] += x
+      k += k & -k
 
   '''bisect_left(acc)'''
   def bisect_left(self, w: int) -> int:
@@ -70,25 +73,37 @@ class FenwickTree:
       s >>= 1
     return i
 
-  def inversion_num(self) -> int:
-    cnt = 0
-    for i in range(self._size):
-      ai = self.get(i)
-      cnt += i - self.sum(ai)
-      self.add(ai, 1)
-    return cnt
-
-  def show_acc(self) -> None:
+  def show(self) -> None:
     print('[' + ', '.join(map(str, [self.pref(i) for i in range(self._size)])) + ']')
 
-  def __str__(self) -> str:
+  @classmethod
+  def inversion_num(self, a: List[int], compress=False) -> int:
+    ans = len(a)
+    if compress or max(a) >= ans:
+      a_ = sorted(set(a))
+      z = {e: i for i, e in enumerate(a_)}
+      fw = FenwickTree(len(a_))
+      for i, e in enumerate(a):
+        fw.add(z[e], 1)
+        ans += i - fw.pref(z[e])
+    else:
+      fw = FenwickTree(ans)
+      for i, e in enumerate(a):
+        fw.add(e, 1)
+        ans += i - fw.pref(e)
+    return ans
+
+  def __str__(self):
     return '[' + ', '.join(map(str, [self.__getitem__(i) for i in range(self._size)])) + ']'
+
+  def __repr__(self):
+    return 'FenwickTree ' + str(self)
 
 
 class FenwickTree2D:
  
-  def __init__(self, h, w, a=[]):
-    '''Build a new FenwickTree2D.'''
+  '''Build a new FenwickTree2D.'''
+  def __init__(self, h: int, w: int, a: List[List[int]]=[]):
     self._h = h + 1
     self._w = w + 1
     self._bit = [[0]*(self._w) for _ in range(self._h)]
@@ -111,8 +126,8 @@ class FenwickTree2D:
         j += j & -j
       h += h & -h
  
+  '''Return sum([0, h) * [0, w)) of a. / O(logH * logW)'''
   def _sum(self, h: int, w: int):
-    '''Return sum([0, h) * [0, w)) of a. / O(logH * logW)'''
     ret = 0
     while h > 0:
       j = w
@@ -124,12 +139,12 @@ class FenwickTree2D:
  
   '''Retrun sum([h1, h2) * [w1, w2)) of a. / O(logH * logW)'''
   def sum(self, h1: int, w1: int, h2: int, w2: int):
-    assert h1 <= h2 and w1 <= w2, f'h1={h1}, h2={h2}, w1={w1}, w2={w2} <- must be h1 <= h2, w1 <= w2.'
+    assert h1 <= h2 and w1 <= w2
     # w1, w2 = min(w1, w2), max(w1, w2)
     # h1, h2 = min(h1, h2), max(h1, h2)
     return self._sum(h2, w2) - self._sum(h2, w1) - self._sum(h1, w2) + self._sum(h1, w1)
 
-  def __str__(self) -> str:
+  def __str__(self):
     ret = []
     for i in range(self._h-1):
       tmp = []
@@ -142,14 +157,14 @@ class FenwickTree2D:
 class FenwickTreeRangeAdd:
 
   '''Build a new FenwickTreeRangeAdd.'''
-  def __init__(self, n):
+  def __init__(self, n: int):
     self._n = n
     self.bit0 = FenwickTree(n+1)
     self.bit1 = FenwickTree(n+1)
 
   '''Add x to a[p]. / O(logN)'''
-  def add(self, p: int, x: int) -> None:
-    self.bit0.add(p, x)
+  def add(self, k: int, x: int) -> None:
+    self.bit0.add(k, x)
 
   '''Add x to range( [l, r) ). / O(logN)'''
   def add_range(self, l: int, r: int, x: int) -> None:
@@ -162,9 +177,8 @@ class FenwickTreeRangeAdd:
   def sum(self, l: int, r: int) -> int:
     return self.bit0.pref(r) + r*self.bit1.pref(r) - self.bit0.pref(l) - l*self.bit1.pref(l)
 
-  def __getitem__(self, i):
-    return self.sum(i, i+1)
+  def __getitem__(self, k: int):
+    return self.sum(k, k+1)
 
   def __str__(self):
     return '[' + ', '.join(map(str, [self.__getitem__(i) for i in range(self._n)])) + ']'
-
