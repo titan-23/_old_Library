c683988f91ebfba46468d1871c59fe1a7e7edcdf
titan <59005133+titanium-22@users.noreply.github.com>
2022-12-07 13:11:36 +0900

    Update topological_sort.py

--- Graph/topological_sort.py
+++ Graph/topological_sort.py
@@ -1,3 +1,6 @@
+# https://github.com/titanium-22/Library/blob/main/Graph/topological_sort.py
+
+
 from typing import List
 from heapq import heapify, heappush, heappop
 from collections import deque
