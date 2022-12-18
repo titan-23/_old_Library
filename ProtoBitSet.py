534434bae0f0e207a73831def7282cc32d821d1b
titan <59005133+titanium-22@users.noreply.github.com>
2022-12-18 23:07:51 +0900

    Update ProtoBitSet.py

--- ProtoBitSet.py
+++ ProtoBitSet.py
@@ -1,5 +1,6 @@
 # https://github.com/titanium-22/Library/blob/main/ProtoBitSet.py
 from typing import List
+
 class ProtoBitSet:
 
   TABLE = [1 << i for i in range(8)]
@@ -18,6 +19,9 @@ class ProtoBitSet:
   def __contains__(self, k: int) -> bool:
     return self._data[k>>3] >> (k & 7) & 1 == 1
 
+  def __getitem__(self, k: int):
+    return self._data[k>>3] >> (k & 7) & 1
+
   def __setitem__(self, k: int, bit: int):
     if bit == 0:
       self._data[k>>3] &= ~ProtoBitSet.TABLE[k & 7]
@@ -27,6 +31,4 @@ class ProtoBitSet:
   def to_l(self) -> List[int]:
     return [(i<<3)+j for i, a in enumerate(self.data) for j in range(8) if a >> j & 1 == 1]
 
-  @property
-  def data(self):
-    return self._data
+
