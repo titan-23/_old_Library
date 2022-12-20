9db47c7e4609d587e17bcb1f0d1977b2b8545811
titan <59005133+titanium-22@users.noreply.github.com>
2022-12-20 14:21:34 +0900

    Update and rename ProtoBitSet.py to NanchatteBitSet.py

--- NanchatteBitSet.py
+++ NanchatteBitSet.py
@@ -1,7 +1,7 @@
-# https://github.com/titanium-22/Library/blob/main/ProtoBitSet.py
+# https://github.com/titanium-22/Library/blob/main/NanchatteBitSet.py
 from typing import List
 
-class ProtoBitSet:
+class NanchatteBitSet:
 
   TABLE = [1 << i for i in range(8)]
 
@@ -11,10 +11,10 @@ class ProtoBitSet:
     self._data = bytearray(self._size)
 
   def add(self, k: int) -> None:
-    self._data[k>>3] |= ProtoBitSet.TABLE[k & 7]
+    self._data[k>>3] |= NanchatteBitSet.TABLE[k & 7]
 
   def discard(self, k: int) -> None:
-    self._data[k>>3] &= ~ProtoBitSet.TABLE[k & 7]
+    self._data[k>>3] &= ~NanchatteBitSet.TABLE[k & 7]
 
   def __contains__(self, k: int) -> bool:
     return self._data[k>>3] >> (k & 7) & 1 == 1
@@ -24,11 +24,10 @@ class ProtoBitSet:
 
   def __setitem__(self, k: int, bit: int):
     if bit == 0:
-      self._data[k>>3] &= ~ProtoBitSet.TABLE[k & 7]
+      self._data[k>>3] &= ~NanchatteBitSet.TABLE[k & 7]
     else:
-      self._data[k>>3] |= ProtoBitSet.TABLE[k & 7]
+      self._data[k>>3] |= NanchatteBitSet.TABLE[k & 7]
 
   def to_l(self) -> List[int]:
     return [(i<<3)+j for i, a in enumerate(self.data) for j in range(8) if a >> j & 1 == 1]
-
 
