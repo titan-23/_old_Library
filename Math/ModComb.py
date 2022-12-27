f44f89ffe7dd2875c18a369b69630148f4fc7151
titan <59005133+titanium-22@users.noreply.github.com>
2022-12-27 22:13:43 +0900

    Add files via upload

---
+++ Math/ModComb.py
@@ -0,0 +1,37 @@
+class ModComb:
+
+  def __init__(self, limit: int, mod: int):
+    # ncr2のみをつかうときなどは、limit=-1とかにすると吉
+    self._mod = mod
+    self._fact = [1, 1]
+    self._factinv = [1, 1]
+    self._inv = [0, 1]
+    self._limit = limit
+    for i in range(2, self._limit+1):
+      self._fact.append(self._fact[-1]*i % self._mod)
+      self._inv.append((-self._inv[self._mod%i] * (self._mod//i)) % self._mod)
+      self._factinv.append(self._factinv[-1] * self._inv[-1] % self._mod)
+
+  '''Return (nCr % mod)'''
+  def ncr(self, n: int, r: int) -> int:
+    if r < 0 or n < r: return 0
+    return self._fact[n] * self._factinv[r] * self._factinv[n-r] % self._mod
+
+  '''Return (nHr % mod)'''
+  def nhr(self, n: int, r: int) -> int:
+    return self.ncr(n+r-1, n-1)
+
+  def ncr2(self, n: int, r: int) -> int:
+    def div_mod(a: int, b: int) -> int:
+      if a % b == 0:
+        return a // b
+      return a * pow(b, self._mod-2, self._mod) % self._mod
+    ret = 1
+    if r > n-r:
+      r = n-r
+    for i in range(r):
+      ret *= n-i
+      ret %= self._mod
+    for i in range(1, r+1):
+      ret = div_mod(ret, i)
+    return ret
