d2ef8bb75db3b6666bcd7a85731ee7f4bf5c8d2c
titan <59005133+titanium-22@users.noreply.github.com>
2023-01-04 18:15:16 +0900

    Update StronglyConnectedComponent.py

--- Graph/StronglyConnectedComponent.py
+++ Graph/StronglyConnectedComponent.py
@@ -1,84 +1,46 @@
-import sys
+from typing import List
 
+'''Strongly Connected Components. / O(N+M)'''
+# コンセプト：
+# - 非再帰
+# - グラフGは単純でなくてもよい
+def get_scc(G: List[List[int]]) -> List[List[int]]:
+  n = len(G)
+  rG = [[] for _ in range(n)]
+  for v in range(n):
+    for x in G[v]:
+      rG[x].append(v)
+  visited = [0] * n
+  toposo = [None] * n
+  now = n
+  for s in range(n):
+    if visited[s]: continue
+    todo = [~s, s]
+    while todo:
+      v = todo.pop()
+      if v >= 0:
+        if visited[v]: continue
+        visited[v] = 2
+        for x in G[v]:
+          if visited[x]: continue
+          todo.append(~x)
+          todo.append(x)
+      else:
+        v = ~v
+        if visited[v] == 1: continue
+        visited[v] = 1
+        now -= 1
+        toposo[now] = v
+  res = []
+  for s in toposo:
+    if not visited[s]: continue
+    todo = [s]
+    for v in todo:
+      visited[v] = 0
+      for x in rG[v]:
+        if not visited[x]: continue
+        visited[x] = 0
+        todo.append(x)
+    res.append(todo)
+  return res
 
-class SCC:
-
-  def __init__(self, G: list):
-    self._n = len(G)
-    self._start, self._elist = self._csr(G)
-    if sys.getrecursionlimit() < self._n+1:
-      sys.setrecursionlimit(self._n+1)
-
-  def _csr(self, G):
-    start = [0] * (self._n + 1)
-    E = []
-    for v in range(self._n):
-      for x in G[v]:
-        E.append((v, x))
-    elist = [0] * len(E)
-    for e0,e1 in E:
-      start[e0+1] += 1
-    for i in range(1, self._n+1):
-      start[i] += start[i-1]
-
-    cnt = start[:]
-    for e0, e1 in E:
-      elist[cnt[e0]] = e1
-      cnt[e0] += 1
-    return start, elist
-
-  def _scc_ids(self):
-
-    now_ord = 0
-    group_num = 0
-
-    visited = []
-
-    low = [0] * self._n
-    ord_ = [-1] * self._n
-    ids = [0] * self._n
-
-    def _dfs(v):
-      nonlocal now_ord, group_num, visited, low, ord_, ids
-
-      low[v] = ord_[v] = now_ord
-      now_ord += 1
-      visited.append(v)
-
-      for i in range(self._start[v], self._start[v+1]):
-        to = self._elist[i]
-
-        if ord_[to] == -1:
-          _dfs(to)
-
-          low[v] = min(low[v], low[to])
-        else:
-          low[v] = min(low[v], ord_[to])
-
-      if low[v] == ord_[v]:
-        while True:
-          u = visited.pop()
-          ord_[u] = self._n
-          ids[u] = group_num
-          if u == v:
-            break
-        group_num += 1
-
-
-    for i in range(self._n):
-      if ord_[i] == -1:
-        _dfs(i)
-
-    for i in range(self._n):
-      ids[i] = group_num - 1 - ids[i]
-
-    return group_num, ids
-
-  def get_scc(self):
-    group_num, ids = self._scc_ids()
-    groups = [[] for _ in range(group_num)]
-    for i in range(self._n):
-      groups[ids[i]].append(i)
-    return groups
-
-  # 閉路検出：len(scc) == nかどうか
