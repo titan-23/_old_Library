33e91c5dd18848feb0a7737d676d5c206acfd6b4
titan <59005133+titanium-22@users.noreply.github.com>
2023-03-28 13:29:24 +0900

    Update FenwickTreeRAQ.py

--- DataStructures/FenwickTree/FenwickTreeRAQ.py
+++ DataStructures/FenwickTree/FenwickTreeRAQ.py
@@ -1,41 +1,65 @@
-from typing import List, Iterable
+from typing import List, Iterable, Union
 
+class FenwickTreeRAQ():
 
-class FenwickTreeRAQ:
+  def __init__(self, _n_or_a: Union[Iterable[int], int]):
+    if isinstance(_n_or_a, int):
+      self.n = _n_or_a
+      self.bit0 = [0] * (_n_or_a + 2)
+      self.bit1 = [0] * (_n_or_a + 2)
+      self.bit_size = self.n + 1
+    else:
+      if not hasattr(_n_or_a, '__len__'):
+        _n_or_a = list(_n_or_a)
+      self.n = len(_n_or_a)
+      self.bit0 = [0] * (self.n + 2)
+      self.bit1 = [0] * (self.n + 2)
+      self.bit_size = self.n + 1
+      for i, e in enumerate(_n_or_a):
+        self.add_range(i, i+1, e)
 
-  '''Build a new FenwickTreeRAQ.'''
-  def __init__(self, n: int, a: Iterable[int]=[]):
-    self._n = n
-    self.bit0 = FenwickTree(n+1)
-    self.bit1 = FenwickTree(n+1)
-    for i, a_ in enumerate(a):
-      self.add(i, a_)
+  def __add(self, bit: List[int], k: int, x: int) -> None:
+    k += 1
+    while k <= self.bit_size:
+      bit[k] += x
+      k += k & -k
 
-  '''Add x to a[k]. / O(logN)'''
+  def __pref(self, bit: List[int], r: int) -> int:
+    ret = 0
+    while r > 0:
+      ret += bit[r]
+      r -= r & -r
+    return ret
+
   def add(self, k: int, x: int) -> None:
+    assert 0 <= k < self.n, \
+        f'IndexError: FenwickTreeRAQ.add({k}, {x}), n={self.n}'
     self.add_range(k, k+1, x)
 
-  '''Add x to [l, r). / O(logN)'''
   def add_range(self, l: int, r: int, x: int) -> None:
-    self.bit0.add(l, -x*l)
-    self.bit0.add(r, x*r)
-    self.bit1.add(l, x)
-    self.bit1.add(r, -x)
+    assert 0 <= l <= r <= self.n, \
+        f'IndexError: FenwickTreeRAQ.add_range({l}, {r}, {x}), l={l},r={r},n={self.n}'
+    self.__add(self.bit0, l, -x*l)
+    self.__add(self.bit0, r, x*r)
+    self.__add(self.bit1, l, x)
+    self.__add(self.bit1, r, -x)
 
-  '''Return sum [l, r). / O(logN)'''
   def sum(self, l: int, r: int) -> int:
-    return self.bit0.pref(r) + r*self.bit1.pref(r) - self.bit0.pref(l) - l*self.bit1.pref(l)
+    assert 0 <= l <= r <= self.n, \
+        f'IndexError: FenwickTreeRAQ.sum({l}, {r}), l={l},r={r},n={self.n}'
+    return self.__pref(self.bit0, r) + r*self.__pref(self.bit1, r) - self.__pref(self.bit0, l) - l*self.__pref(self.bit1, l)
 
-  def to_l(self) -> List[int]:
-    return [self.sum(i, i+1) for i in range(self._n)]
+  def tolist(self) -> List[int]:
+    return [self.sum(i, i+1) for i in range(self.n)]
 
   def __getitem__(self, k: int):
+    assert 0 <= k < self.n, \
+        f'IndexError: FenwickTreeRAQ.__getitem__({k}), n={self.n}'
     return self.sum(k, k+1)
 
   def __str__(self):
-    return '[' + ', '.join(map(str, (self.__getitem__(i) for i in range(self._n)))) + ']'
+    return '[' + ', '.join(map(str, (self.sum(i, i+1) for i in range(self.n)))) + ']'
 
   def __repr__(self):
-    return 'FenwickTreeRAQ' + str(self)
-
+    return f'FenwickTreeRAQ({self})'
 
