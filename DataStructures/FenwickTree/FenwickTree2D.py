99fe650b807e626f98f2354b146c6ebcdfa18650
titan <59005133+titanium-22@users.noreply.github.com>
2023-03-28 13:29:00 +0900

    Update FenwickTree2D.py

--- DataStructures/FenwickTree/FenwickTree2D.py
+++ DataStructures/FenwickTree/FenwickTree2D.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 class FenwickTree2D:
  
   '''Build a new FenwickTree2D. / O(HW)'''
@@ -9,6 +8,7 @@ class FenwickTree2D:
     self._w = w + 1
     self._bit = [[0]*(self._w) for _ in range(self._h)]
     if a:
+      assert len(a) == h and len(a[0]) == w
       self._build(a)
 
   def _build(self, a):
@@ -28,6 +28,9 @@ class FenwickTree2D:
         j += j & -j
       h += h & -h
  
+  def set(self, h: int, w: int, x) -> None:
+    self.add(h, w, x-self.get(h, w))
+
   '''Return sum([0, h) * [0, w)) of a. / O(logH * logW)'''
   def _sum(self, h: int, w: int):
     ret = 0
@@ -47,13 +50,12 @@ class FenwickTree2D:
     # h1, h2 = min(h1, h2), max(h1, h2)
     return self._sum(h2, w2) - self._sum(h2, w1) - self._sum(h1, w2) + self._sum(h1, w1)
 
+  def get(self, h: int, w: int):
+    return self.sum(h, h+1, w, w+1)
+
   def __str__(self):
     ret = []
     for i in range(self._h-1):
       ret.append(', '.join(map(str, ((self.sum(i, j, i+1, j+1)) for j in range(self._w-1)))))
     return '[ ' + '\n  '.join(map(str, ret)) + ' ]'
-
-  def __repr__(self):
-    return 'FenwickTree2D\n' + str(self)
-
 
