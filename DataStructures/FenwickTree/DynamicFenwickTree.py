08206138d12fdb71e5349bf6011d6abd0e1cd4b9
titan <59005133+titanium-22@users.noreply.github.com>
2023-03-28 13:28:35 +0900

    Update DynamicFenwickTree.py

--- DataStructures/FenwickTree/DynamicFenwickTree.py
+++ DataStructures/FenwickTree/DynamicFenwickTree.py
@@ -1,37 +1,45 @@
-class DynamicFenwickTree:
- 
-  def __init__(self, n: int):
-    self._size = n+1
+from typing import Optional
+
+class DynamicFenwickTree():
+
+  def __init__(self, u: int):
+    '''Build DynamicFenwickTree [0, u).'''
+    assert isinstance(u, int), \
+        f'TypeError: DynamicFenwickTree({u}), {u} must be int'
+    self._u = u
     self._tree = {}
-    self._s = 1 << n.bit_length()
- 
+    self._s = 1 << (u-1).bit_length()
+
   def add(self, k: int, x: int) -> None:
+    assert 0 <= k < self._u, \
+        f'IndexError: DynamicFenwickTree.add({k}, {x}), u={self._u}'
     k += 1
-    while k <= self._size:
+    while k <= self._u:
       if k in self._tree:
         self._tree[k] += x
       else:
         self._tree[k] = x
       k += k & -k
- 
-  '''Return sum([0, r)) of a. / O(logN)'''
+
   def pref(self, r: int) -> int:
+    assert 0 <= r <= self._u, \
+        f'IndexError: DynamicFenwickTree.pref({r}), u={self._u}'
     ret = 0
     while r > 0:
       if r in self._tree:
         ret += self._tree[r]
       r -= r & -r
     return ret
- 
-  def sum(self, l, r):
+
+  def sum(self, l: int, r: int) -> int:
+    assert 0 <= l <= r <= self._u, \
+        f'IndexError: DynamicFenwickTree.sum({l}, {r}), u={self._u}'
     return self.pref(r) - self.pref(l)
- 
-  '''bisect_left(acc)'''
-  def bisect_left(self, w: int) -> int:
-    i = 0
-    s = self._s
-    while s > 0:
-      if i+s <= self._size:
+
+  def bisect_left(self, w: int) -> Optional[int]:
+    i, s = 0, self._s
+    while s:
+      if i+s <= self._u:
         if i+s in self._tree and self._tree[i+s] < w:
           w -= self._tree[i+s]
           i += s
@@ -39,13 +47,11 @@ class DynamicFenwickTree:
           i += s
       s >>= 1
     return i if w else None
- 
-  '''bisect_right(acc)'''
+
   def bisect_right(self, w: int) -> int:
-    i = 0
-    s = self._s
-    while s > 0:
-      if i+s <= self._size:
+    i, s = 0, self._s
+    while s:
+      if i+s <= self._u:
         if i+s in self._tree and self._tree[i+s] <= w:
           w -= self._tree[i+s]
           i += s
@@ -53,4 +59,7 @@ class DynamicFenwickTree:
           i += s
       s >>= 1
     return i
- 
+
+  def __str__(self):
+    return str(self._tree)
+
