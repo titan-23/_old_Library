05e4b0cf4c4c98b5b105c260db91358ceb812ade
titan <59005133+titanium-22@users.noreply.github.com>
2023-04-03 10:32:54 +0900

    Update Mo.py

--- DataStructures/Mo/Mo.py
+++ DataStructures/Mo/Mo.py
@@ -1,44 +1,47 @@
 from typing import Callable
+from itertools import chain
 from __pypy__ import newlist_hint
+from math import sqrt, ceil
 
 class Mo():
-
-  BUCKET_SIZE = 866
 
   def __init__(self, n: int, q: int):
     # n:= 平方分割する列全体の大きさ
     # q:= クエリの数
     self.n = n
     self.q = q
+    self.bucket_size = ceil(sqrt(3)*n/sqrt(2*q))
+    # self.bucket_size = 866
     self.bit = max(n, q).bit_length()
     self.msk = (1 << self.bit) - 1
-    self.bucket = [newlist_hint(10) for _ in range(n//Mo.BUCKET_SIZE+1)]
-    self.query_count = 0
+    self.bucket = [newlist_hint(self.bucket_size//10) for _ in range(n//self.bucket_size+1)]
+    self.cnt = 0
 
   def add_query(self, l: int, r: int) -> None:
-    self.bucket[l//Mo.BUCKET_SIZE].append((((r<<self.bit)+l)<<self.bit)+self.query_count)
-    self.query_count += 1
+    assert 0 <= l <= r <= self.n, \
+        f'IndexError: Mo.add_query({l}, {r}), self.n={self.n}'
+    self.bucket[l//self.bucket_size].append((((r<<self.bit)|l)<<self.bit)|self.cnt)
+    self.cnt += 1
 
   def run(self, add: Callable[[int], None], delete: Callable[[int], None], out: Callable[[int], None]) -> None:
+    assert self.cnt == self.q
     bucket, bit, msk = self.bucket, self.bit, self.msk
     for i, b in enumerate(bucket):
       b.sort(reverse=i & 1)
     nl, nr = 0, 0
-    for b in bucket:
-      for rli in b:
-        r = rli >> bit >> bit
-        l = rli >> bit & msk
-        while nl > l:
-          nl -= 1
-          add(nl)
-        while nl < l:
-          delete(nl)
-          nl += 1
-        while nr < r:
-          add(nr)
-          nr += 1
-        while nr > r:
-          nr -= 1
-          delete(nr)
-        out(rli & msk)
+    for rli in chain(*bucket):
+      r, l = rli >> bit >> bit, rli >> bit & msk
+      while nl > l:
+        nl -= 1
+        add(nl)
+      while nl < l:
+        delete(nl)
+        nl += 1
+      while nr < r:
+        add(nr)
+        nr += 1
+      while nr > r:
+        nr -= 1
+        delete(nr)
+      out(rli & msk)
 
