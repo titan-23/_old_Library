58e47604d408b07b27bfa7ba57748cc13ecb1ab2
titanium-22 <titan.kaneda@gmail.com>
2023-04-11 00:47:08 +0900

    fch

--- Others/Random.py
+++ Others/Random.py
@@ -1,8 +1,20 @@
-# aaaaa
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+import math
+import random
+from bisect import bisect_right, bisect_left
+from itertools import product, permutations, combinations, combinations_with_replacement 
+from collections import deque, defaultdict, Counter
+from heapq import heapify, heappush, heappop
+from functools import lru_cache, reduce
+inf = float('inf')
+def error(*args, sep=' ', end='\n'):
+  print(*args, sep=sep, end=end, file=sys.stderr)
+# mod = 1000000007
+# mod = 998244353
+DIR = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+sys.setrecursionlimit(7*10**5)
 
-fadafda
-fadafdaafa
-fadafdaafawfa
-fadafdaafawfaawfe
-ansf
+#  -----------------------  #
+
 
